19cd73192428f1bb9f0d482e3d93a33c8a45d3db
TheLoooser <ottifant99@hotmail.com>
2023-07-11 19:45:13 +0200

    Implement pause

--- main.py
+++ main.py
@@ -22,12 +22,48 @@ display_surface = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Game")
 
 
-def close_game_when_exiting():
-    # Close game upon exiting the window
-    for event in pygame.event.get():
-        if event.type == QUIT:
-            pygame.quit()
-            sys.exit()
+def blur_surface(surface, amount):
+    if amount < 1:
+        raise ValueError("")
+
+    scale = 1.0/float(amount)
+    surface_size = surface.get_size()
+    scale_size = (int(surface_size[0] * scale), int(surface_size[1] * scale))
+    surf = pygame.transform.smoothscale(surface, scale_size)
+    surf = pygame.transform.smoothscale(surf, surface_size)
+    return surf
+
+
+def paused():
+    # Darken pause background
+    s = pygame.Surface((WIDTH, HEIGHT))  # the size of your rect
+    s.set_alpha(50)  # alpha level
+    s.fill((50, 50, 50))  # this fills the entire surface
+    display_surface.blit(s, (0, 0))
+
+    # Print pause text
+    font = pygame.font.SysFont("arial", 50)
+    text = font.render('Pause', True, (200, 200, 200))
+    # text.set_alpha(200)
+    text_rect = text.get_rect()
+    text_rect.center = ((display_surface.get_width() / 2), (display_surface.get_height() / 2))
+    display_surface.blit(text, text_rect)
+
+    pause = True
+    while pause:
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == KEYDOWN:
+                if event.key == K_ESCAPE:
+                    pause = False
+
+        # button("Continue", 150, 450, 100, 50, green, bright_green, unpause)
+        # button("Quit", 550, 450, 100, 50, red, bright_red, quitgame)
+
+        pygame.display.update()
+        FramePerSec.tick(15)
 
 
 def draw_game(all_sprites):
@@ -70,7 +106,16 @@ def run():
 
     # Main Game Loop
     while True:
-        close_game_when_exiting()
+        for event in pygame.event.get():
+            # Close game upon exiting the window
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == KEYDOWN:
+                if event.key == K_ESCAPE:  # pause game
+                    display_surface.blit(blur_surface(display_surface, 2), (0, 0))
+                    paused()
+
         draw_game(all_sprites)
         i, j, previous_cell, cells = player.highlight_player_cell(cells, previous_cell, grid)
         next_move, old_direction, new_direction, old_field = player.move_player(next_move, old_direction, grid, i, j,
